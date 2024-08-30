bb6a6c8443553afe27ffd719a3d6b1dea625d88b
rejinhacker <144967373+rejinhacker@users.noreply.github.com>
2024-08-30 15:30:05 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from "./components/Home";  // Import Home component
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Removed 'Link' import
+import Home from "./components/Home";  
 import SCP2 from "./components/SCP2";
 import SCP3 from "./components/SCP3";
 import SCP4 from "./components/SCP4";
 import SCP5 from "./components/SCP5";
 import SCP6 from "./components/SCP6";
-import Navbar from "./components/Navbar"; // Import Navbar component
-import "./App.css"; // Import styling
+import Navbar from "./components/Navbar"; 
+import "./App.css";
 
 function App() {
     const [menuOpen, setMenuOpen] = useState(false);
 
     const toggleMenu = (isOpen) => {
-        setMenuOpen(isOpen === undefined ? !menuOpen : isOpen); // If no argument, toggle, else set state
+        setMenuOpen(isOpen === undefined ? !menuOpen : isOpen);
     };
 
     return (
         <Router>
             <div className="App">
-                <Navbar menuOpen={menuOpen} toggleMenu={toggleMenu} /> {/* Use Navbar component */}
+                <Navbar menuOpen={menuOpen} toggleMenu={toggleMenu} />
                 <main>
                     <Routes>
-                        <Route path="/" element={<Home />} /> {/* Display Home component */}
+                        <Route path="/" element={<Home />} />
                         <Route path="/scp2" element={<SCP2 />} />
                         <Route path="/scp3" element={<SCP3 />} />
                         <Route path="/scp4" element={<SCP4 />} />
